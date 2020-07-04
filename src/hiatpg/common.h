7cb1c1b95e0ce317476df31db81497e8fd46e489
yvhangfish <yvhangfish@outlook.com>
2020-07-04 11:27:49 +0800

    add basic data structure.

--- src/hiatpg/common.h
+++ src/hiatpg/common.h
@@ -5,6 +5,11 @@
 #ifndef HIATPG_COMMON_H
 #define HIATPG_COMMON_H
 
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 using GateId = int;
 
 enum Value {
@@ -33,6 +38,38 @@ enum EventDir {
     BOTH
 };
 
+struct Gate {
+    GateId gateId;
+    GateType type;
+    vector<Gate*> fanins;
+    vector<Gate*> fanouts;
+    string name;
+};
 
+struct Fault {
+    GateId gateId;
+    int index;
+    Value value;
+    bool detected;
+};
+
+struct AtpgValue {
+    Value goodVal;
+    Value faultyVal;
+};
+
+class NetList {
+    vector<Gate*> gates;
+    unordered_map<string, Gate*> name2GatePointer;
+};
+
+class FaultList {
+    vector<Fault*> faults;
+};
+
+class AtpgEngine {
+    unordered_map<GateId, AtpgValue> window;
+    unordered_map<GateId, Value> testCube;
+};
 
 #endif //HIATPG_COMMON_H
