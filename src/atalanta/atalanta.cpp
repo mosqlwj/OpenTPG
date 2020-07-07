7df867f5652d401f96efcf88ec29b7093d840ce5
fish <yvhangfish@outlook.com>
2020-07-07 20:29:42 +0800

    add cmdline

--- src/atalanta/atalanta.cpp
+++ src/atalanta/atalanta.cpp
@@ -5,11 +5,25 @@
 
 #include <stdio.h>
 #include <vector>
+#include "cmdline.h"
 
 using namespace hiatpg;
 
 int main(int argc, char** argv)
 {
+    cmdline::parser options;
+    // 加入指定类型的输入參数
+    // 第一个參数：长名称
+    // 第二个參数：短名称（‘\0‘表示没有短名称）
+    // 第三个參数：參数描写叙述
+    // 第四个參数：bool值，表示该參数是否必须存在（可选。默认值是false）
+    // 第五个參数：參数的默认值（可选，当第四个參数为false时该參数有效）
+    options.add<string>("bench", 'b', "bench file name", true, "");
+//    options.add<string>("fault", 'f', "fault file name", true, "");
+    options.parse_check(argc, argv);
+
+    string benchFile = options.get<string>("bench");
+
 	int res = 0;
 	fstream bench, pat;
 
@@ -17,7 +31,7 @@ int main(int argc, char** argv)
     Params* paras;
 
     paras = new Params();
-    bench.open("c17.bench", ios::in);
+    bench.open(benchFile, ios::in);
     pat.open("c17.pat", ios::out);
     paras->setBenchStream(bench.rdbuf());
     paras->setSPatternStream(pat.rdbuf());
