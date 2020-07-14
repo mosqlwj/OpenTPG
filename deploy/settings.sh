331aa70df3bf887b156ab9304eab32608695480e
libbylg <libbylg@126.com>
2020-07-14 18:51:33 +0800

    support mult file to replace

--- deploy/settings.sh
+++ deploy/settings.sh
@@ -5,8 +5,12 @@ SELFDIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 export  HADOOP_HOME="${SELFDIR}/hadoop"
 export  PATH="${PATH}:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin"
 
-sed "s#\${HADOOP_HOME}#${HADOOP_HOME}#g"    -i  "${HADOOP_HOME}/etc/hadoop/core-site.sh"
-sed "s#\${JAVA_HOME}#${JAVA_HOME}#g"        -i  "${HADOOP_HOME}/etc/hadoop/hadoop-env.sh"
+templateFiles=$(cd "${HADOOP_HOME}/etc/hadoop" && ls)
+
+for f in ${templateFiles} ; do
+    sed "s#\${HADOOP_HOME}#${HADOOP_HOME}#g"    -i  "${HADOOP_HOME}/etc/hadoop/${f}"    &&  \
+    sed "s#\${JAVA_HOME}#${JAVA_HOME}#g"        -i  "${HADOOP_HOME}/etc/hadoop/${f}"
+done
 
 if [[ ! -d "${HADOOP_HOME}/data" ]]; then
     "${HADOOP_HOME}/bin/hdfs"   namenode    -format
