c1637d7bfe796fbed53dfd18b71fba4365a3779f
libbylg <libbylg@126.com>
2020-07-15 23:46:24 +0800

    add test app

--- deploy/test-app.c
+++ deploy/test-app.c
@@ -1,14 +1,18 @@
-//#!gcc test-echo.c -o test-echo -g -rdynamic
+//#!gcc test-app.c -o test-app -g -rdynamic
 #include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, const char* argv[])
 {
+    if (argc < 2) {
+        printf("Missing parameter: test-app <mapper>|<reducer>");
+        return -1;
+    }
     char buffer[4096];
     while (!feof(stdin) && !ferror(stdin)) {
         const char* ptr = fgets(buffer, sizeof(buffer), stdin);
         if (NULL != ptr) {
-            fprintf(stdout, "%d: ", getpid());
+            fprintf(stdout, "%s %d: ", argv[1], getpid());
             fputs(ptr, stdout);
         }
     }
