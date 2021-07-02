321c499bdd76f8cb882b3175eb857fb5fa94a323
fish <yvhangfish@outlook.com>
2021-07-02 17:09:02 +0800

    adjust directory structure.

---
+++ HiAtpg/src/Params.h
@@ -0,0 +1,32 @@
+#ifndef __HIPARSER_PARAMS_H__
+#define __HIPARSER_PARAMS_H__
+
+#include "cmdline.h"
+
+class Params {
+private:
+    Params();
+    std::string netlistFile;
+    static Params* instance;
+
+public:
+    static Params* getInstance();
+    const std::string &getNetlistFile() const { return netlistFile; }
+
+    void parseCheck(int argc, char *argv[]) {
+        cmdline::parser options;
+        // 加入指定类型的输入參数
+        // 第一个參数：长名称
+        // 第二个參数：短名称（‘\0‘表示没有短名称）
+        // 第三个參数：參数描写叙述
+        // 第四个參数：bool值，表示该參数是否必须存在（可选。默认值是false）
+        // 第五个參数：參数的默认值（可选，当第四个參数为false时该參数有效）
+        options.add<std::string>("netlist",
+                                 'n',
+                                 "The netlist file name such as ./c17.bench",
+                                 true);
+        options.parse_check(argc, argv);
+        netlistFile = options.get<std::string>("netlist");
+    }
+};
+#endif
