cb06ed5a2d0a13b486e7cb0daef292a284b085f5
fish <yvhangfish@outlook.com>
2021-08-13 10:36:09 +0800

    reorder pi.

--- HiAtpg/src/Faultlist.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by luolijun on 2021/8/12.
-//
-
-#include "Faultlist.h"
--- HiAtpg/src/main.cpp
+++ HiAtpg/src/main.cpp
@@ -1,6 +1,5 @@
 #include "ATPGDriverDefault.h"
 #include "CubeHandlerWriteFile.h"
-#include "Faultlist.h"
 #include "Netlist.h"
 #include "Params.h"
 #include "printers.h"
@@ -14,15 +13,11 @@ int main(int argc, char** argv)
     Netlist netlist;
     netlist.Parse(params->GetNetlistFile());
 
-    //  生成fault
-    Faultlist faultlist;
-
     //  生成cube
     std::unique_ptr<ATPGDriver> atpgDriver(CreateATPGDriver());
     CubeHandlerWriteFile cubeOutputPrinter(params->GetCubeDumpFile());
     atpgDriver->SetupCubeOutput(&cubeOutputPrinter);
     atpgDriver->SetupNetlist(&netlist);
-    atpgDriver->SetupFaultlist(&faultlist);
     int ret = atpgDriver->Prepare();
     if (ret != 0) {
         return -1;
